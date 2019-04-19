e9094892e33ef8f8dbc73a5f9e7d7ee42f561625
heqijun <578391255@qq.com>
2019-04-19 17:06:03 +0800

    no message

--- NativeMixFlutterApp/app/src/main/java/com/heqijun/nativemixflutterapp/app.java
+++ NativeMixFlutterApp/app/src/main/java/com/heqijun/nativemixflutterapp/app.java
@@ -1,6 +1,57 @@
 package com.heqijun.nativemixflutterapp;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+import com.heqijun.nativemixflutterapp.boost.PageRouter;
+import com.taobao.idlefish.flutterboost.FlutterBoostPlugin;
+import com.taobao.idlefish.flutterboost.interfaces.IPlatform;
+
+import java.util.Map;
+
 import io.flutter.app.FlutterApplication;
 
-public class app extends FlutterApplication {
+/**
+ * @author heqijun
+ * @description
+ * @date 2019/4/19 5:04 PM
+ */
+public class App extends FlutterApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterBoostPlugin.init(new IPlatform() {
+            @Override
+            public Application getApplication() {
+                return App.this;
+            }
+
+            /**
+             * 获取应用入口的Activity,这个Activity在应用交互期间应该是一直在栈底的
+             * @return
+             */
+            @Override
+            public Activity getMainActivity() {
+                return MainActivity.sRef.get();
+            }
+
+            @Override
+            public boolean isDebug() {
+                return true;
+            }
+
+            @Override
+            public boolean startActivity(Context context, String url, int requestCode) {
+                return PageRouter.openPageByUrl(context,url,requestCode);
+            }
+
+            @Override
+            public Map getSettings() {
+                return null;
+            }
+        });
+
+    }
 }
